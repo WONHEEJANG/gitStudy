bb77431dee69382ec497f11d2a6e9d01bf46acfc
WONHEEJANG <35652460+WONHEEJANG@users.noreply.github.com>
2022-12-08 19:59:58 +0900

    2022.12.08. 20:00
    - 초기화면 데이터 출력 완료
    - HomeScreen 컴포넌트 완료
    - HomeScreen props 데이터 전달 정상

--- firebase_study/App.js
+++ firebase_study/App.js
@@ -1,24 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { 
-  TextInput, 
-  StyleSheet, 
-  Text, 
-  SafeAreaView, 
-  FlatList, 
-  TouchableOpacity, 
-  Image, 
-  View, 
-  Dimensions, 
-  Button 
-} from 'react-native'
-
+import HomeScreen from "./Screens/HomeScreen";
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue, set } from "firebase/database";
-import DetailModal from "./Components/DetailModal";
 
 
-const deviceWidth = Dimensions.get('screen').width
-const deviceHeight = Dimensions.get('screen').height
+
 const firebaseConfig = {
   apiKey: "AIzaSyBLeUe16wlLroQmzmj31BtdOo21ZLcNStM",
   authDomain: "fir-test-5ce77.firebaseapp.com",
@@ -32,23 +18,6 @@ const firebaseConfig = {
 
 
 
-const Item = ({ item, onPress }) => (
-  <View height={deviceHeight * 0.25} width={deviceWidth * 0.4} alignItems={'center'}
-    margin={deviceWidth * 0.025}>
-    <TouchableOpacity onPress={onPress}>
-      <Image width={100} height={100} source={{
-        uri: item.ImageURL.length > 0 ? item.ImageURL : null
-      }} style={styles.profileImage} />
-      <Text style={styles.boldText}>{`${item.Name} ${item.Title}`}</Text>
-      <Text style={styles.plainText}>{`${item.Department}`}</Text>
-      <Text style={styles.plainText}>{`${item.Team}`}</Text>
-    </TouchableOpacity>
-  </View>
-);
-
-
-
-
 const App = () => {
 
   const [data, setData] = useState([])
@@ -56,110 +25,19 @@ const App = () => {
   const db = getDatabase(app);
   const employeeDBRef = ref(db, 'employees/');
 
-  const [fillteredData, setFillteredData] = useState([])
-  const [modalVisible, setModalVisible] = useState(false)
-  const [selectedData, setSelectedData] = useState({
-    Name: "",
-    Title: "",
-    Rank: "",
-    Major: "",
-    YearOfAdmission: "",
-    Department: "",
-    Team: "",
-    ImageURL: "null"
-  });
-
-  const modalCallBack = (requestClose) => {
-    setModalVisible(false)
-  }
-
-
-  const onChangeText = text => {
-    console.log("onchange!")
-    setFillteredData(data.filter((item) => item.Name.includes(text) ||
-      item.Title.includes(text) ||
-      `${item.YearOfAdmission}`.includes(text) ||
-      item.Major.includes(text) ||
-      item.Department.includes(text) ||
-      item.Team.includes(text) ||
-      item.Rank.includes(text)
-    ))
-  }
-
-  const renderItem = ({ item }) => {
-    return (<Item item={item} onPress={() => {
-      setSelectedData(item)
-      setModalVisible(!modalVisible)
-    }} />);
-  };
-
-
   useEffect(() => {
     onValue(employeeDBRef, (snapshot) => {
       const data_snapshot = snapshot.val();
       setData(data_snapshot)
-      setFillteredData(data_snapshot)
       console.log("FETCHING COMPLETE")
+      console.log(data_snapshot)
     });
   }, []);
 
   return (
-    <SafeAreaView style={styles.safeAreaView}>
-        <DetailModal modalVisible={modalVisible} selectedData={selectedData} callback = {modalCallBack}/>
-        <TextInput style={styles.textInput} onChangeText={onChangeText} selectionColor={'black'} />
-        <FlatList data={fillteredData}
-            showsVerticalScrollIndicator={false} renderItem={renderItem} keyExtractor={(item, index) => index} numColumns={2}
-            columnWrapperStyle={{
-                justifyContent: 'space-between',
-                marginBottom: 0
-            }} styles={styles.flatList}
-            width={deviceWidth * 0.9} />
-    </SafeAreaView>
+    <HomeScreen userDB = {data}/>
   );
 };
-
-
-
-const styles = StyleSheet.create({
-  textInput: {
-      width: '80%',
-      height: 40,
-      borderWidth: 1,
-      borderColor: 'lightgray',
-      backgroundColor: 'lightgray',
-      borderRadius: 10,
-      marginTop: 20,
-      marginBottom: 20,
-      paddingLeft: 15
-  },
-  safeAreaView: {
-      alignItems: 'center',
-      flex: 1.0,
-      backgroundColor : 'white'
-  },
-  profileImage: {
-      width: 100,
-      height: 100,
-      borderRadius: 100
-  },
-  flatList: {
-      marginBottom: 200
-  },
-  plainText: {
-      textAlign: 'center',
-      paddingTop: 7,
-      fontSize: 13,
-      color : 'gray'
-  },
-  boldText: {
-      textAlign: 'center',
-      paddingTop: 7,
-      fontWeight: "bold",
-      fontSize: 15
-
-  },
-
-});
 
 
 export default App;
---
+++ firebase_study/Screens/HomeScreen.js
@@ -0,0 +1,134 @@
+import React, { useState, useEffect } from "react";
+import { TextInput, StyleSheet, Text, SafeAreaView, FlatList, TouchableOpacity, Image, View, Dimensions } from 'react-native'
+
+import DetailModal from "../Components/DetailModal";
+
+const deviceWidth = Dimensions.get('screen').width
+const deviceHeight = Dimensions.get('screen').height
+
+
+const Item = ({ item, onPress }) => (
+    <View height={deviceHeight * 0.25}
+        width={deviceWidth * 0.4}
+        alignItems={'center'}
+        margin={deviceWidth * 0.025}>
+        <TouchableOpacity onPress={onPress}>
+            <Image width={100} height={100} source={{
+                uri: item.ImageURL.length > 0 ? item.ImageURL : null
+            }} style={styles.profileImage} />
+            <Text style={styles.boldText}>{`${item.Name} ${item.Title}`}</Text>
+            <Text style={styles.plainText}>{`${item.Department}`}</Text>
+            <Text style={styles.plainText}>{`${item.Team}`}</Text>
+        </TouchableOpacity>
+    </View>
+);
+
+function HomeScreen({ navigation, userDB }) {
+
+    const [data, setData] = useState(userDB)
+    const [fillteredData, setFillteredData] = useState(data)
+
+    const [modalVisible, setModalVisible] = useState(false)
+    const [selectedData, setSelectedData] = useState({
+        Name: "",
+        Title: "",
+        Rank: "",
+        Major: "",
+        YearOfAdmission: "",
+        Department: "",
+        Team: "",
+        ImageURL: "null"
+    });
+
+    const modalCallBack = (requestClose) => {
+        setModalVisible(false)
+    }
+
+    // For TextInput
+    const onChangeText = text => {
+        console.log("onchange!")
+        setFillteredData(data.filter((item) => item.Name.includes(text) ||
+            item.Title.includes(text) ||
+            `${item.YearOfAdmission}`.includes(text) ||
+            item.Major.includes(text) ||
+            item.Department.includes(text) ||
+            item.Team.includes(text) ||
+            item.Rank.includes(text)
+        ))
+    }
+
+    // For FlatList
+    const renderItem = ({ item }) => {
+        return (<Item item={item} onPress={() => {
+            setSelectedData(item)
+            setModalVisible(!modalVisible)
+        }} />);
+    };
+
+    useEffect(() => {
+        setData(userDB);
+        setFillteredData(userDB);
+    }, [userDB]);
+
+    return (
+    <SafeAreaView style={styles.safeAreaView}>
+        <DetailModal modalVisible={modalVisible} selectedData={selectedData} callback={modalCallBack} />
+        <TextInput style={styles.textInput} onChangeText={onChangeText} selectionColor={'black'} />
+        <FlatList data={fillteredData}
+            showsVerticalScrollIndicator={false}
+            renderItem={renderItem}
+            keyExtractor={(item, index) => index}
+            numColumns={2}
+            width={deviceWidth * 0.9}
+            columnWrapperStyle={{
+                justifyContent: 'space-between',
+                marginBottom: 0
+            }} styles={styles.flatList}
+        />
+    </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    textInput: {
+        width: '80%',
+        height: 40,
+        borderWidth: 1,
+        borderColor: 'lightgray',
+        backgroundColor: 'lightgray',
+        borderRadius: 10,
+        marginTop: 20,
+        marginBottom: 20,
+        paddingLeft: 15
+    },
+    safeAreaView: {
+        alignItems: 'center',
+        flex: 1.0,
+        backgroundColor: 'white'
+    },
+    profileImage: {
+        width: 100,
+        height: 100,
+        borderRadius: 100
+    },
+    flatList: {
+        marginBottom: 200
+    },
+    plainText: {
+        textAlign: 'center',
+        paddingTop: 7,
+        fontSize: 13,
+        color: 'gray'
+    },
+    boldText: {
+        textAlign: 'center',
+        paddingTop: 7,
+        fontWeight: "bold",
+        fontSize: 15
+
+    },
+
+});
+
+
+export default HomeScreen;
