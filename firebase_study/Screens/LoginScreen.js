8f5eea11f91d09569839d871725282c888243935
WONHEEJANG <35652460+WONHEEJANG@users.noreply.github.com>
2022-12-25 11:44:20 +0900

    221225 11:44
    blurView

--- firebase_study/Screens/LoginScreen.js
+++ firebase_study/Screens/LoginScreen.js
@@ -1,54 +1,116 @@
 import React, { useState, useEffect } from "react";
-import { TextInput, StyleSheet, Text, SafeAreaView, Alert, TouchableOpacity, Image, View, Dimensions, Button } from 'react-native'
+import { TextInput, StyleSheet, Text, TouchableOpacity, Image, View } from 'react-native'
+import { BlurView } from "@react-native-community/blur";
 
 
 const LoginScreen = ({ callback }) => {
-    return (
-        <SafeAreaView style={styles.safeAreaView}>
-            <View style={styles.loginForm}>
-                <Text> 사번 </Text>
-                <TextInput style={styles.textInput} selectionColor={'black'} />
-                <Text> 비밀번호 </Text>
-                <TextInput style={styles.textInput} selectionColor={'black'} />
-                <Button
-                    title="Press me"
-                    onPress={() => {
-                        // Alert.alert('Simple Button pressed')
-                        callback()
-                    }}
-                />
-            </View>
-        </SafeAreaView>
+    return (<>
+        <View style={styles.bgView}>
+            <Image style={styles.bg} source={require('../icons/login_bg.jpeg')} />
+            <BlurView style={styles.loginBg} blurAmount={25} blurType={'light'}>
+                <View style={styles.loginForm}>
+                    <TextInput style={styles.textInput} placeholder="사번" selectionColor={'black'} placeholderTextColor={'gray'} />
+                    <TextInput style={styles.textInput} placeholder="비밀번호" selectionColor={'black'} placeholderTextColor={'gray'} marginBottom={50}/>
+                    
+                    <TouchableOpacity style={styles.loginBtn} onPress={() => {
+                            callback()
+                        }}>
+                        <Text style={styles.loginBtnTxt}>로그인</Text>
+                    </TouchableOpacity>
+                </View>
+            </BlurView>
+
+        </View>
+    </>
     )
 }
 
+// <SafeAreaView style={styles.safeAreaView}>
+//             <View style={styles.loginForm}>
+// <Text> 사번 </Text>
+// <TextInput style={styles.textInput} selectionColor={'black'} />
+// <Text> 비밀번호 </Text>
+// <TextInput style={styles.textInput} selectionColor={'black'} />
+// <Button
+//     title="Press me"
+//     onPress={() => {
+//         // Alert.alert('Simple Button pressed')
+//         callback()
+//     }}
+// />
+//             </View>
+//         </SafeAreaView>
+
 const styles = StyleSheet.create({
-    safeAreaView: {
+    bgView: {
         alignItems: 'center',
         flex: 1.0,
-        backgroundColor: 'white',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        zIndex: 1,
+        elevation: 1
+    },
+
+    bg: {
+        alignItems: 'center',
+        flex: 1.0,
+        justifyContent: 'center',
+        zIndex: 2,
+        elevation: 2
     },
 
     textInput: {
         width: '80%',
         height: 40,
         borderWidth: 1,
-        borderColor: 'lightgray',
-        backgroundColor: 'lightgray',
+        borderColor: 'white',
+        backgroundColor: 'white',
+        opacity: 0.6,
         borderRadius: 10,
         marginTop: 20,
         marginBottom: 20,
-        paddingLeft: 15
+        paddingLeft: 15,
+    },
+
+    loginBtn: {
+        backgroundColor: 'purple',
+        opacity: 0.6,
+        width: '60%',
+        height: 40,
+        borderRadius: 10,
+        alignItems:'center',
+        justifyContent:'center',
     },
 
     loginForm: {
         alignItems: 'center',
         width: '100%',
-        height: '50%',
-        backgroundColor: 'pink',
-        justifyContent: 'center'
+        height: '100%',
+        position: 'absolute',
+        justifyContent: 'center',
+        borderColor: 'white',
+        shadowColor: 'white',
+        opacity: 0.9,
+        borderRadius: 40,
+        borderWidth:0.4,
+        zIndex: 3,
+        elevation: 3
     },
+    loginBg: {
+        alignItems: 'center',
+        borderRadius: 40,
+        width: '80%',
+        height: '50%',
+        position: 'absolute',
+        justifyContent: 'center',
+        zIndex: 2,
+        elevation: 2
+    },
+    loginBtnTxt:{
+        color:'white',
+        fontWeight:'900',
+        fontSize:15
+        
+    }
 });
 
 export default LoginScreen;
